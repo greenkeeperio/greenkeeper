078226bf0f5b19b8e0c16103af66e1badd7f8ce1
Stephan Bönnemann <stephan@boennemann.me>
2015-08-16 22:09:28 +0200

    feat(login): implement login

--- src/login.js
+++ src/login.js
@@ -1,5 +1,43 @@
 var log = require('npmlog')
+var open = require('open')
+var request = require('request')
+var randomString = require('random-string')
 
-module.exports = function () {
+var rc = require('./lib/rc')
+
+module.exports = function (config) {
   log.verbose('login', 'starting command')
+
+  if (config.token && !config.force) {
+    log.error('login', 'Already logged in. Use --force to continue.')
+    process.exit(1)
+  }
+
+  var id = randomString({length: 16})
+  log.verbose('login', 'id', id)
+
+  log.verbose('login', 'Getting token from API and opening GitHub login')
+  request({
+    method: 'POST',
+    json: true,
+    url: config.api + 'tokens',
+    timeout: 1000 * 60 * 60, // wait 1h
+    body: {
+      id: id
+    }
+  }, function (err, res, data) {
+    if (err) return log.error('login', 'Request failed', err)
+
+    if (!(res.statusCode === 200 && data.token)) {
+      return log.error('login', 'Login failed', res, data)
+    }
+
+    rc.set('token', data.token)
+    log.info('login', 'Logged in')
+  })
+
+  var url = config.api + 'login?id=' + id
+
+  log.info('login', 'Open ' + url)
+  open(url)
 }
