c730e012679acf65ef961ecefce1fb1bf49815b5
Stephan Bönnemann <stephan@boennemann.me>
2015-08-16 22:09:05 +0200

    feat: merge cli flags with rcfile

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 #!/usr/bin/env node
 
-var nopt = require('nopt')
-var log = require('npmlog')
+var url = require('url')
+
+var _ = require('lodash')
 var emoji = require('node-emoji')
+var hideSecrets = require('hide-secrets')
+var log = require('npmlog')
+var nopt = require('nopt')
 
-var commands = require('./commands')
+var rc = require('./lib/rc')
 var pkg = require('../package.json')
+var commands = require('./commands')
 
-var parsed = nopt({
+var rcFlags = rc.get()
+
+var cliFlags = nopt({
   version: Boolean,
   help: Boolean,
+  api: String,
   loglevel: [
     'silly',
     'verbose',
@@ -32,22 +40,33 @@ var parsed = nopt({
   quiet: ['--loglevel', 'warn']
 })
 
-log.level = parsed.loglevel || 'warn'
+log.level = cliFlags.loglevel || rcFlags.loglevel || 'info'
 log.headingStyle = {fg: 'white'}
 log.heading = emoji.get('palm_tree')
 
-if (parsed.version) {
+var flags = _.assign({}, rcFlags, cliFlags)
+
+flags.api = url.parse(flags.api || 'https://api.greenkeeper.io/').format()
+
+log.silly('cli', 'rc arguments', _.omit(hideSecrets(rcFlags), 'argv'))
+log.silly('cli', 'cli arguments', _.omit(hideSecrets(cliFlags), 'argv'))
+log.verbose('cli', 'arguments', _.omit(hideSecrets(flags), 'argv'))
+
+if (flags.version) {
   console.log(pkg.version || 'development')
   process.exit(0)
 }
 
-var command = commands.all[(parsed.argv.remain || []).shift().toLowerCase()]
+var command = commands.all[(flags.argv.remain || []).shift().toLowerCase()]
 
-if (parsed.help || !command) {
+if (flags.help || !command) {
   console.log('Usage: greenkeeper <command>\n')
   console.log('where <command> is one of:')
   console.log('    ', commands.join(', '))
+  console.log('\n' + emoji.get('palm_tree'))
   process.exit(0)
 }
 
-require('./' + command)(parsed, pkg)
+if (flags.force) log.warn('cli', 'using --force')
+
+require('./' + command)(flags, pkg)
