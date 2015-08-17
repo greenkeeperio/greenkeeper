5ba60660050d1e591d4e45fa49ed4209e73931d4
Stephan Bönnemann <stephan@boennemann.me>
2015-08-17 19:47:20 +0200

    feat(whoami): initial implementation

--- src/whoami.js
+++ src/whoami.js
@@ -1,5 +1,21 @@
 var log = require('npmlog')
+var request = require('request')
 
-module.exports = function () {
+module.exports = function (flags) {
   log.verbose('whoami', 'starting command')
+
+  if (!flags.token) process.exit(0)
+
+  log.http('whoami', 'Sending request')
+  request({
+    url: flags.api + 'whoami',
+    json: true,
+    headers: {
+      Authorization: 'Bearer ' + flags.token
+    }
+  }, function (err, res, data) {
+    if (data.name) return console.log(data.name)
+
+    log.error('whoami', err || res)
+  })
 }
