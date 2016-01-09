8a1e846bb9ad149e30bca86126a27fab09948529
Leonard Lamprecht <leo@users.noreply.github.com>
2016-01-09 17:28:03 +0100

    fix(support): intercom chat is used for paid plans

--- src/support.js
+++ src/support.js
@@ -5,12 +5,12 @@ module.exports = function (flags) {
   log.verbose('support', 'starting command')
 
   if (!flags.token) {
-    log.info('support', 'Not logged in. Opening the general support repo.')
+    log.info('support', 'Not logged in. Opening GitHub issues.')
     open('https://github.com/greenkeeperio/greenkeeper')
     process.exit(1)
   }
 
-  log.info('support', 'Opening Intercom chat in browser')
+  log.info('support', 'Opening GitHub issues (free plan) or Intercom')
 
   open(flags.api + 'support?access_token=' + flags.token)
 }
