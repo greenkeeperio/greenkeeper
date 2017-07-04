3963a0752a323850c84d56a9f6003647c1c8728f
Julia Krüger <juliakbln@gmail.com>
2017-07-04 10:54:36 +0200

    feat(user-created-pr): add payment status if required

--- jobs/github-event/pull_request/opened.js
+++ jobs/github-event/pull_request/opened.js
@@ -1,12 +1,15 @@
 const dbs = require('../../../lib/dbs')
 const getConfig = require('../../../lib/get-config')
+const { hasBilling } = require('../../../lib/payments')
+const githubQueue = require('../../../lib/github-queue')
 
 module.exports = async function (data) {
   const { repositories } = await dbs()
-  const { pull_request: pullRequest, repository } = data
+  const { pull_request: pullRequest, repository, installation } = data
   const repositoryId = String(repository.id)
   const prDocId = `${repositoryId}:pr:${pullRequest.id}`
   const repoDoc = await repositories.get(repositoryId)
+  const [owner, repo] = repository.full_name.split('/')
   const config = getConfig(repoDoc)
 
   const wasCreatedByGreenkeeper = pullRequest.user.type === 'Bot' && pullRequest.user.login.substr(0, 11) === 'greenkeeper'
@@ -30,4 +33,18 @@ module.exports = async function (data) {
       createdByUser: true
     }
   )
+  const ghqueue = githubQueue(installation.id)
+
+  const accountHasBilling = await hasBilling(repository.owner.id)
+  if (repoDoc.private && !accountHasBilling) {
+    await ghqueue.write(github => github.repos.createStatus({
+      owner,
+      repo,
+      sha: pullRequest.head.sha,
+      state: 'pending',
+      context: 'greenkeeper/payment',
+      description: 'Payment required, merging will have no effect',
+      target_url: 'https://account.greenkeeper.io/'
+    }))
+  }
 }
