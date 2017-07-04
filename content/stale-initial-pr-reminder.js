3797d0b3fa7fde4668e308475f58f5baf13b2c5f
Alex Feyerke <espy@users.noreply.github.com>
2017-07-04 10:24:40 +0200

    refactor(content): reworded pr reminder, removed line breaks

--- content/stale-initial-pr-reminder.js
+++ content/stale-initial-pr-reminder.js
@@ -3,12 +3,9 @@ const md = require('./template')
 module.exports = () => md`
 Hey there 👋,
 
-we noticed that this PR is not merged yet. Just to let you know _Greenkeeper will
-not be enabled_ and you won't receive updates for your dependencies, if you don't merge
-this PR.
+we noticed that this PR isn’t merged yet. Just to let you know: if you don't merge this PR, _Greenkeeper will not be enabled_ on this repo, and you won't receive updates for your dependencies.
 
-In case you don't want to enable Greenkeeper, just close or ignore this PR, we
-won't nag you again. ✌️
+If don't want to enable Greenkeeper here, just close or ignore this PR, we won't nag you again. ✌️
 
 Have a great day! 🌴
 `
