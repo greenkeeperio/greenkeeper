3383d626a5d570adb356839303663fb92625097e
Robin Mehner <robin@coding-robin.de>
2017-08-23 12:12:41 +0200

    feat(timeout-issue): link to custom GitHub host if user has set the environment variable

---
+++ test/content/timeout-issue.js
@@ -0,0 +1,37 @@
+const { test } = require('tap')
+
+function cleanCache (module) {
+  delete require.cache[require.resolve(module)]
+}
+
+function requireFresh (module) {
+  cleanCache(module)
+  return require(module)
+}
+
+test('timeout issue content', async t => {
+  t.afterEach(() => {
+    delete process.env.GITHUB_HOST
+    delete process.env.GITHUB_URL
+    cleanCache('../../lib/env')
+  })
+
+  t.test('includes the link to the initial branch on the custom github host', async t => {
+    process.env.GITHUB_HOST = 'https://enterprise.github/api/v3/'
+    const content = requireFresh('../../content/timeout-issue')
+
+    const issueContent = content({fullName: 'finnp/abc'})
+    t.match(issueContent, /enterprise\.github\/finnp\/abc/, 'includes the link to the repo at the custom host')
+
+    t.end()
+  })
+
+  t.test('includes the link to the initial branch on the regular github host', async t => {
+    const content = requireFresh('../../content/timeout-issue')
+
+    const issueContent = content({fullName: 'finnp/abc'})
+    t.match(issueContent, /github\.com\/finnp\/abc/, 'includes the link to the repo at github.com')
+
+    t.end()
+  })
+})
