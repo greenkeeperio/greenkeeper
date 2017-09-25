885efd467821f67451ae3f09e3be38a5b6123a5f
Gregor <gregor@martynus.net>
2017-09-25 14:40:17 -0700

    fix(cancel-stripe-subscription): await upsert response

--- jobs/cancel-stripe-subscription.js
+++ jobs/cancel-stripe-subscription.js
@@ -5,15 +5,8 @@ const upsert = require('../lib/upsert')
 
 module.exports = async function ({ accountId, stripeSubscriptionId }) {
   const { payments } = await dbs()
-  await stripe.subscriptions.del(
-    stripeSubscriptionId,
-    async (err, confirmation) => {
-      if (err) {
-        throw err
-      }
-      await upsert(payments, accountId, {
-        stripeSubscriptionId: null
-      })
-    }
-  )
+  await stripe.subscriptions.del(stripeSubscriptionId)
+  await upsert(payments, accountId, {
+    stripeSubscriptionId: null
+  })
 }
