094e9ffa0b4164b7192d1b99c73f9a2afb880880
Julia Krüger <juliakbln@gmail.com>
2017-09-25 16:31:00 +0200

    feat(jobs) added markedplace_purchase changed

---
+++ jobs/github-event/marketplace_purchase/changed.js
@@ -0,0 +1,11 @@
+const dbs = require('../../../lib/dbs')
+const upsert = require('../../../lib/upsert')
+
+module.exports = async function ({ marketplace_purchase }) {
+  const { payments } = await dbs()
+  const accountId = String(marketplace_purchase.account.id)
+
+  await upsert(payments, accountId, {
+    plan: marketplace_purchase.plan.name.toLowerCase().replace(' ', '')
+  })
+}
