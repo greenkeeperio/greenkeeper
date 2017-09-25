dd19da7ef6df7bb84d1167383f4a8946b8775862
Julia Krüger <juliakbln@gmail.com>
2017-09-25 16:30:22 +0200

    feat(jobs): added marketplace_purchase cancelled

---
+++ test/jobs/github-event/marketplace_purchase/cancelled.js
@@ -0,0 +1,57 @@
+const { test, tearDown } = require('tap')
+const dbs = require('../../../../lib/dbs')
+const worker = require('../../../../jobs/github-event/marketplace_purchase/cancelled')
+
+const removeIfExists = async (db, id) => {
+  try {
+    return await db.remove(await db.get(id))
+  } catch (e) {
+    if (e.status !== 404) {
+      throw e
+    }
+  }
+}
+
+test('marketplace canceled', async t => {
+  t.test('change entry in payments database to `free`', async t => {
+    const { payments } = await dbs()
+    await payments.put({
+      _id: '444',
+      plan: 'team'
+    })
+
+    const newJobs = await worker({
+      marketplace_purchase: {
+        account: {
+          type: 'Organization',
+          id: 444,
+          login: 'GitHub'
+        },
+        plan: {
+          id: 9,
+          name: 'Team',
+          description: 'A really, super professional-grade CI solution',
+          monthly_price_in_cents: 9999,
+          yearly_price_in_cents: 11998,
+          price_model: 'flat-rate',
+          unit_name: null,
+          bullets: [
+            'This is the first bullet of the plan',
+            'This is the second bullet of the plan'
+          ]
+        }
+      }
+    })
+
+    t.notOk(newJobs, 'no new job scheduled')
+
+    const payment = await payments.get('444')
+    t.is(payment.plan, 'free', 'plan: free')
+    t.end()
+  })
+})
+
+tearDown(async () => {
+  const { payments } = await dbs()
+  await removeIfExists(payments, '444')
+})
