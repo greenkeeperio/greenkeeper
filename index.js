2153034e279489b27c89072258e7f8dc9dc73b85
Julia Krüger <juliakbln@gmail.com>
2017-09-26 16:15:49 +0200

    fix: add cancel-stripe-subscription to jobsWithoutOwners

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ require('./lib/rollbar')
 
   async function consume (job) {
     const data = JSON.parse(job.content.toString())
-    const jobsWithoutOwners = ['registry-change', 'stripe-event', 'schedule-stale-initial-pr-reminders', 'reset']
+    const jobsWithoutOwners = ['registry-change', 'stripe-event', 'schedule-stale-initial-pr-reminders', 'reset', 'cancel-stripe-subscription']
     if (jobsWithoutOwners.includes(data.name)) {
       return queueJob(data.name, job)
     }
