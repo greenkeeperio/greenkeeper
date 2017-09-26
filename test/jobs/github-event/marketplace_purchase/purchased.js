96778afcd25ff7632f13d6147c58c3dcdf9778a4
Julia Krüger <juliakbln@gmail.com>
2017-09-26 15:52:27 +0200

    reafctor(tests): extract removeIfExists function to helper file

--- test/jobs/github-event/marketplace_purchase/purchased.js
+++ test/jobs/github-event/marketplace_purchase/purchased.js
@@ -2,16 +2,7 @@ const { test, tearDown } = require('tap')
 const nock = require('nock')
 const dbs = require('../../../../lib/dbs')
 const worker = require('../../../../jobs/github-event/marketplace_purchase/purchased')
-
-const removeIfExists = async (db, id) => {
-  try {
-    return await db.remove(await db.get(id))
-  } catch (e) {
-    if (e.status !== 404) {
-      throw e
-    }
-  }
-}
+const removeIfExists = require('../../../remove-if-exists.js')
 
 nock.disableNetConnect()
 nock.enableNetConnect('localhost')
