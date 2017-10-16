a99ecb6e0884d21cb433da5a473a239b907ea437
Julia Krüger <juliakbln@gmail.com>
2017-10-16 17:15:23 +0200

    chore(send-stripe-cancel-survey): add survey

--- content/stripe-cancel-survey.js
+++ content/stripe-cancel-survey.js
@@ -4,6 +4,8 @@ module.exports = () => md`
 Hey there 👋,
 
 we noticed that you canceled your Greenkeeper subscription.
+To be able to improve our product and services in the future, we would be happy if you could answer a few quick questions from the cancellation survey below.
+http://bit.ly/2zs3ouq
 
 Have a great day! 🌴
 `
