f8213335d6923c3bd002b296a73d717de7616127
Julia Krüger <juliakbln@gmail.com>
2017-10-19 15:11:22 +0200

    feat(integration-installation-repositories): add logging for removed job

--- jobs/github-event/integration_installation_repositories/removed.js
+++ jobs/github-event/integration_installation_repositories/removed.js
@@ -1,14 +1,16 @@
 const _ = require('lodash')
+const Log = require('gk-log')
 
 const dbs = require('../../../lib/dbs')
 const statsd = require('../../../lib/statsd')
 const { maybeUpdatePaymentsJob } = require('../../../lib/payments')
 
 module.exports = async function ({ installation, repositories_removed }) {
-  const { repositories: reposDb } = await dbs()
+  const { repositories: reposDb, logs } = await dbs()
   const accountId = String(installation.account.id)
   const repoIds = _.map(repositories_removed, repo => String(repo.id))
-
+  const log = Log({logsDb: logs, accountId: installation.account.id, repoSlug: null, context: 'integration-installation-repositories-removed'})
+  log.info('started', { repositories_removed })
   // branches and prs will only be deleted on a complete uninstall
   const repositories = _(
     (await reposDb.query('by_account', {
@@ -21,11 +23,14 @@ module.exports = async function ({ installation, repositories_removed }) {
     .map(doc => _.assign(doc, { _deleted: true }))
     .value()
 
+  log.info('database: add `_deleted: true` to selected repositories', { repositories })
   statsd.decrement('repositories', repositories.length)
 
   await reposDb.bulkDocs(repositories)
 
   const hasPrivateRepos = repositories.some(repo => repo.private)
+
+  log.success('starting maybeUpdatePaymentsJob', { hasPrivateRepos })
 
   return maybeUpdatePaymentsJob(accountId, hasPrivateRepos)
 }
