c223fd2c578c227b308a8b08f24bdd3ffb654faf
Julia Krüger <juliakbln@gmail.com>
2017-10-20 15:14:31 +0200

    feat(marketplace-purchase-purchased): add logging

--- jobs/github-event/marketplace_purchase/purchased.js
+++ jobs/github-event/marketplace_purchase/purchased.js
@@ -1,10 +1,18 @@
+const Log = require('gk-log')
+
 const dbs = require('../../../lib/dbs')
 const upsert = require('../../../lib/upsert')
 const normalizePlanName = require('../../../lib/normalize-plan-name')
 
 module.exports = async function ({ marketplace_purchase }) {
-  const { payments } = await dbs()
-
+  const { payments, logs } = await dbs()
+  const log = Log({
+    logsDb: logs,
+    accountId: marketplace_purchase.account.id,
+    repoSlug: null,
+    context: 'marketplace-purchase-created'
+  })
+  log.info('started', { marketplace_purchase })
   const accountId = String(marketplace_purchase.account.id)
   let paymentDoc
 
@@ -14,11 +22,14 @@ module.exports = async function ({ marketplace_purchase }) {
 
   try {
     paymentDoc = await payments.get(String(accountId))
+    log.success('database: paymentDoc created', { paymentDoc })
   } catch (error) {
+    log.error('database: no paymentDoc created', { error })
     if (error.status !== 404) throw error
   }
 
   if (paymentDoc && paymentDoc.stripeSubscriptionId) {
+    log.info('sheduled `cancel-stripe-subscription` job')
     return {
       data: {
         name: 'cancel-stripe-subscription',
