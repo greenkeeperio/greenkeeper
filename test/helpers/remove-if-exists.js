bffa40be8cab3976fd3434360bc046aefbb544c2
Robin Mehner <robin@coding-robin.de>
2018-01-30 12:05:46 +0100

    test: allow remove-if-exists to take an array as parameter and add some docs

--- test/helpers/remove-if-exists.js
+++ test/helpers/remove-if-exists.js
@@ -1,6 +1,13 @@
+/**
+ * Tries to delete a document in Couch, but does not fail if it doesn't exist
+ *
+ * @param {PouchDB} db - the PouchDB object of the database
+ * @param {(string[]|...string)} ids - the ids of the documents to be deleted
+ */
 module.exports = async function (db, ...ids) {
+  const idsToDelete = Array.isArray(ids[0]) ? ids[0] : ids
   return Promise.all(
-    ids.map(async (id) => {
+    idsToDelete.map(async (id) => {
       try {
         return await db.remove(await db.get(id))
       } catch (e) {
