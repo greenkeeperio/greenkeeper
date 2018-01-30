b771be9990e1545f260c0636c710ef2ee67128df
Robin Mehner <robin@coding-robin.de>
2018-01-30 12:09:43 +0100

    refactor(initialPR): check for private first, then for enterprise

--- jobs/create-initial-pr.js
+++ jobs/create-initial-pr.js
@@ -64,7 +64,7 @@ module.exports = async function (
   }))
   log.info('github: set greenkeeper/verify status')
 
-  if (!env.IS_ENTERPRISE && repodoc.private) {
+  if (repodoc.private && !env.IS_ENTERPRISE) {
     const billingAccount = await getActiveBilling(accountId)
     const hasBillingAccount = !!billingAccount
     const accountNeedsMarketplaceUpgrade = await getAccountNeedsMarketplaceUpgrade(accountId)
