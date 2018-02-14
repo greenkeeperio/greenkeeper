52b24d8d669a459425f2ef7879e54f96ef192a25
Julia Krüger <juliakbln@gmail.com>
2018-02-14 16:19:58 +0100

    test(create-branch): add test for monorepo use case

--- test/lib/create-branch.js
+++ test/lib/create-branch.js
@@ -17,7 +17,7 @@ function ghToken (nocked) {
 }
 
 describe('create branch', async () => {
-/* test('change one file (package.json)', async () => {
+  test('change one file (package.json)', async () => {
     ghToken(nock('https://api.github.com'))
       .get('/repos/owner/repo/contents/package.json')
       .query({ ref: 'master' })
@@ -164,7 +164,6 @@ describe('create branch', async () => {
 
     expect(sha).toEqual('789beef2')
   })
-  */
 
   const testThreeData = {
     'package.json': {
@@ -180,10 +179,7 @@ describe('create branch', async () => {
   }
 
   test('change multiple monorepo files (package.json, backend/package.json)', async () => {
-    console.log('################################################')
-    console.log('################################################')
-    console.log('################################################')
-    console.log('TEST 3')
+    expect.assertions(12)
 
     ghToken(nock('https://api.github.com'))
       .get('/repos/owner/repo/contents/package.json')
@@ -204,61 +200,36 @@ describe('create branch', async () => {
           sha: '123abc2'
         }
       })
-      .post('/repos/owner/repo/git/trees'/*, {
-        tree: [
-          {
-            path: 'package.json',
-            content: 'TESTDATA',
-            mode: '100644',
-            type: 'blob'
-          }
-        ],
-        base_tree: '123abc2'
-      } */)
+      .post('/repos/owner/repo/git/trees')
       .reply(201, (uri, requestBody) => {
-        console.log('requestBody', requestBody)
+        expect(JSON.parse(requestBody).tree[0].path).toEqual('package.json')
+        expect(JSON.parse(requestBody).tree[0].content).toEqual('{"dependencies":{"react":"2.0.0"}}')
         return {sha: 'def457'}
       })
-      .post('/repos/owner/repo/git/trees', {
-        tree: [
-          {
-            path: 'readme.md',
-            content: 'testdata',
-            mode: '100644',
-            type: 'blob'
-          }
-        ],
-        base_tree: '789beef2'
+      .post('/repos/owner/repo/git/trees')
+      .reply(201, (uri, requestBody) => {
+        expect(JSON.parse(requestBody).tree[0].path).toEqual('backend/package.json')
+        expect(JSON.parse(requestBody).tree[0].content).toEqual('{"dependencies":{"react":"2.0.0"}}')
+        return {sha: 'def458'}
       })
-      .reply(201, {
-        sha: '890abc'
+      .post('/repos/owner/repo/git/commits')
+      .reply(201, (uri, requestBody) => {
+        expect(JSON.parse(requestBody).tree).toEqual('def457')
+        expect(JSON.parse(requestBody).parents[0]).toEqual('123abc2')
+        expect(JSON.parse(requestBody).message).toEqual('pkg')
+        return {sha: '789beef1'}
       })
-      .post('/repos/owner/repo/git/commits', {
-        message: 'pkg',
-        tree: 'def457',
-        parents: ['123abc2']
+      .post('/repos/owner/repo/git/commits')
+      .reply(201, (uri, requestBody) => {
+        expect(JSON.parse(requestBody).tree).toEqual('def458')
+        expect(JSON.parse(requestBody).parents[0]).toEqual('789beef1')
+        expect(JSON.parse(requestBody).message).toEqual('pkg2')
+        return {sha: '789beef2'}
       })
-      .reply(201, {
-        sha: '789beef2'
+      .post('/repos/owner/repo/git/refs')
+      .reply(201, (uri, requestBody) => {
+        expect(JSON.parse(requestBody).sha).toEqual('789beef2')
       })
-      .post('/repos/owner/repo/git/commits', {
-        message: 'readme',
-        tree: '890abc',
-        parents: ['789beef2']
-      })
-      .reply(201, {
-        sha: '789beef2'
-      })
-      .post('/repos/owner/repo/git/refs', {
-        ref: 'refs/heads/testBranch',
-        sha: '789beef2'
-      })
-      .reply(201)
-
-    /*
-      - does branch have 2 correct commits?
-      - are the transforms correct? (by checking content in reply requestBody)
-    */
 
     const sha = await createBranch({
       installationId: 123,
@@ -274,16 +245,12 @@ describe('create branch', async () => {
         },
         {
           path: 'backend/package.json',
-          message: 'pkg',
-          transform: (old, path) => {
-            console.log('old', old ? JSON.parse(old) : 'no old :(')
-            console.log('path', path)
-            return createTransformFunction('dependencies', 'react', '2.0.0', console)(old)
-          }
+          message: 'pkg2',
+          transform: (old, path) => createTransformFunction('dependencies', 'react', '2.0.0', console)(old)
         }
       ]
     })
 
-    // t.equal(sha, '789beef2', 'sha')
+    expect(sha).toEqual('789beef2')
   })
 })
