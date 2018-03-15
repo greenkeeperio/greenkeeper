230d5c1d8c08a2810310d3d2efcded39e097a4b7
Julia Krüger <juliakbln@gmail.com>
2018-03-15 11:39:19 +0100

    WIP: validate greenkeeper.json

---
+++ test/lib/validate-greenkeeper-json.js
@@ -0,0 +1,60 @@
+const {validate} = require('../../lib/validate-greenkeeper-json')
+
+test('valid', () => {
+  const file = {
+    groups: {
+      frontend: {
+        packages: [
+          'packages/frontend/package.json',
+          'packages/lalalalala/package.json'
+        ]
+      },
+      backend: {
+        packages: [
+          'packages/backend/package.json'
+        ]
+      }
+    }
+  }
+  const result = validate(file)
+  expect(result.error).toBeFalsy()
+})
+
+test('invalid: no group/s', () => {
+  const file = {
+    frontend: {
+      packages: [
+        'packages/frontend/package.json',
+        'packages/lalalalala/package.json'
+      ]
+    },
+    backend: {
+      packages: [
+        'packages/backend/package.json'
+      ]
+    }
+  }
+  const result = validate(file)
+  expect(result.error).toBeTruthy()
+  expect(result.error.name).toEqual('ValidationError')
+  expect(result.error.details[0].message).toMatch(/"groups" is required/)
+})
+
+test('invalid: no packages', () => {
+  const file = {
+    groups: {
+      frontend: {
+        ignore: [
+          'lodash'
+        ]
+      },
+      backend: {
+        packages: [
+          'packages/backend/package.json'
+        ]
+      }
+    }
+  }
+  const result = validate(file)
+  expect(result.error).toBeFalsy()
+})
