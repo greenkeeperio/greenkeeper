0932e84b7f90a9552c2a2c152fac01065aeb6612
Julia Krüger <juliakbln@gmail.com>
2018-03-15 12:43:53 +0100

    feat(validate-greenkeeper-json): validation with joi

--- test/lib/validate-greenkeeper-json.js
+++ test/lib/validate-greenkeeper-json.js
@@ -20,6 +20,90 @@ test('valid', () => {
   expect(result.error).toBeFalsy()
 })
 
+test('valid with subgroup level ignore', () => {
+  const file = {
+    groups: {
+      frontend: {
+        ignore: [
+          'lodash'
+        ],
+        packages: [
+          'packages/frontend/package.json',
+          'packages/lalalalala/package.json'
+        ]
+      },
+      backend: {
+        packages: [
+          'packages/backend/package.json'
+        ]
+      }
+    }
+  }
+  const result = validate(file)
+  expect(result.error).toBeFalsy()
+})
+
+test('invalid: groupname has invalid characters', () => {
+  const file = {
+    groups: {
+      'front-end': {
+        ignore: [
+          'lodash'
+        ],
+        packages: [
+          'packages/frontend/package.json',
+          'packages/lalalalala/package.json'
+        ]
+      },
+      backend: {
+        packages: [
+          'packages/backend/package.json'
+        ]
+      }
+    }
+  }
+  const result = validate(file)
+  expect(result.error).toBeTruthy()
+  expect(result.error.details[0].message).toMatch(/"front-end" is not allowed/)
+})
+
+test('invalid: absolute paths are not allowed', () => {
+  const file = {
+    groups: {
+      frontend: {
+        packages: [
+          '/packages/frontend/package.json'
+        ]
+      }
+    }
+  }
+  const result = validate(file)
+  expect(result.error).toBeTruthy()
+  expect(result.error.name).toEqual('ValidationError')
+  expect(result.error.details[0].path).toEqual([ 'groups', 'frontend', 'packages', 0 ])
+  expect(result.error.details[0].context.value).toEqual('/packages/frontend/package.json')
+  expect(result.error.details[0].message).toMatch(/fails to match the required pattern/)
+})
+
+test('invalid: path is not ending on `package.json`', () => {
+  const file = {
+    groups: {
+      frontend: {
+        packages: [
+          'packages/frontend/package.json',
+          'packages/frontend/'
+        ]
+      }
+    }
+  }
+  const result = validate(file)
+  expect(result.error).toBeTruthy()
+  expect(result.error.name).toEqual('ValidationError')
+  expect(result.error.details[0].path).toEqual([ 'groups', 'frontend', 'packages', 1 ])
+  expect(result.error.details[0].context.value).toEqual('packages/frontend/')
+  expect(result.error.details[0].message).toMatch(/fails to match the required pattern/)
+})
+
 test('invalid: no group/s', () => {
   const file = {
     frontend: {
@@ -58,4 +142,6 @@ test('invalid: no packages', () => {
   }
   const result = validate(file)
   expect(result.error).toBeTruthy()
+  expect(result.error.name).toEqual('ValidationError')
+  expect(result.error.details[0].message).toMatch(/"packages" is required/)
 })
