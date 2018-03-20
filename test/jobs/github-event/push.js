aeecfe92ba173f327a21680d485ecdec108a71a1
hulkoba <jacoba.brandner@web.de>
2018-03-20 16:10:22 +0100

    test(push): separate mororepo tests

--- test/jobs/github-event/push.js
+++ test/jobs/github-event/push.js
@@ -832,6 +832,13 @@ describe('github-event push', async () => {
     expect(repo.headSha).toEqual('deadbeef')
     expect(repo.enabled).toBeFalsy()
   })
+})
+
+describe('github-event push: monorepo', () => {
+  beforeEach(() => {
+    jest.resetModules()
+    nock.cleanAll()
+  })
 
   test('monorepo: 2 package.jsons in 2 groups modified (666)', async () => {
     const configFileContent = {
@@ -969,7 +976,6 @@ describe('github-event push', async () => {
   })
 
   /*
-    TODO:
     Deletions:
     - [x] group deleted -> should delete all group’s branches
     - [x] file in group deleted -> should delete all group’s branches
@@ -3317,25 +3323,25 @@ describe('github-event push', async () => {
     expect(frontend.referenceDeleted).toBeFalsy()
     expect(repo.headSha).toEqual('9049f1265b7d61be4a8904a9a27120d2064dab3b')
   })
+})
 
-  afterAll(async () => {
-    const { repositories, payments } = await dbs()
+afterAll(async () => {
+  const { repositories, payments } = await dbs()
 
-    await removeIfExists(repositories, '444', '444A', '445', '445A', '446', '447', '448', '444:branch:1234abcd', '444:branch:1234abce', '444A:branch:1234abcd', '444A:branch:1234abce', '555', '666',
-    '777', '777:branch:1234abcd', '777:branch:1234abce', '777:branch:1234abcf', '777:branch:1234abcg',
-    '777A', '777A:branch:1234abca', '777A:branch:1234abcb', '777A:branch:1234abcc',
-    '888', '888:branch:1234abca', '888:branch:1234abcb',
-    '999', '999:branch:1234abca', '999:branch:1234abcb',
-    '1111',
-    '1112', '1112:branch:1234abca', '1112:branch:1234abcb',
-    '1113', '1113:branch:1234abca', '1113:branch:1234abcb',
-    '1114', '1114:branch:1234abca', '1114:branch:1234abcb',
-    '1115', '1115:branch:1234abca', '1115:branch:1234abcb',
-    '1116', '1116:branch:1234abca', '1116:branch:1234abcb',
-    '1117', '1117:branch:1234abca',
-    '1118', '1118:branch:1234abca')
-    await removeIfExists(payments, '123')
-  })
+  await removeIfExists(repositories, '444', '444A', '445', '445A', '446', '447', '448', '444:branch:1234abcd', '444:branch:1234abce', '444A:branch:1234abcd', '444A:branch:1234abce', '555', '666',
+  '777', '777:branch:1234abcd', '777:branch:1234abce', '777:branch:1234abcf', '777:branch:1234abcg',
+  '777A', '777A:branch:1234abca', '777A:branch:1234abcb', '777A:branch:1234abcc',
+  '888', '888:branch:1234abca', '888:branch:1234abcb',
+  '999', '999:branch:1234abca', '999:branch:1234abcb',
+  '1111',
+  '1112', '1112:branch:1234abca', '1112:branch:1234abcb',
+  '1113', '1113:branch:1234abca', '1113:branch:1234abcb',
+  '1114', '1114:branch:1234abca', '1114:branch:1234abcb',
+  '1115', '1115:branch:1234abca', '1115:branch:1234abcb',
+  '1116', '1116:branch:1234abca', '1116:branch:1234abcb',
+  '1117', '1117:branch:1234abca',
+  '1118', '1118:branch:1234abca')
+  await removeIfExists(payments, '123')
 })
 
 function encodePkg (pkg) {
