314b57cd78036cf2e0359924fadb0a568b66d544
hulkoba <fuchspippi@web.de>
2018-04-10 11:50:41 +0200

    chore(test): remove nock imports where it is not used

--- test/utils/initial-branch-utils.js
+++ test/utils/initial-branch-utils.js
@@ -1,11 +1,6 @@
-const nock = require('nock')
-
 const dbs = require('../../lib/dbs')
 const removeIfExists = require('../helpers/remove-if-exists')
 const { cleanCache } = require('../helpers/module-cache-helpers')
-
-nock.disableNetConnect()
-nock.enableNetConnect('localhost')
 
 describe('create initial branch', () => {
   beforeEach(() => {
