c4fea2d7e0a5ad9cb12f926167373495b3f9bf59
hulkoba <fuchspippi@web.de>
2018-04-11 12:23:28 +0200

    fix(create-initial-branch): ignore missing package.jsons in greenkeeper.json

--- utils/initial-branch-utils.js
+++ utils/initial-branch-utils.js
@@ -14,13 +14,15 @@ const registryUrl = env.NPM_REGISTRY
   }]
 */
 function getDependenciesFromPackageFiles (packagePaths, packageJsonContents) {
-  return _.uniqWith(_.flatten(packagePaths.map(path => {
+  return _.compact(_.uniqWith(_.flatten(packagePaths.map(path => {
     return _.flatten(
       ['dependencies', 'devDependencies', 'optionalDependencies'].map(type => {
-        return _.map(packageJsonContents[path][type], (version, name) => ({ name, version, type }))
+        if (packageJsonContents[path]) {
+          return _.map(packageJsonContents[path][type], (version, name) => ({ name, version, type }))
+        }
       })
     )
-  })), _.isEqual)
+  })), _.isEqual))
 }
 
 // add npm package data to dependency info from previous function
