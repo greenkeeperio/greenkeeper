3b06f12746003540dfecae695f41a5cb6a6249e8
Julia Krüger <juliakbln@gmail.com>
2018-04-16 13:13:32 +0200

    fix(database): remove invisiable character

--- couchdb/repositories/_design/pr_open_by_dependency_and_group/views/pr_open_by_dependency_and_group/map.js
+++ couchdb/repositories/_design/pr_open_by_dependency_and_group/views/pr_open_by_dependency_and_group/map.js
@@ -1,4 +1,4 @@
 function (doc) {
-  if (doc.type !== 'pr' || doc.initial || doc.state !== 'open') return
+  if (doc.type !== 'pr' || doc.initial || doc.state !== 'open') return
   emit([doc.repositoryId, doc.dependency, doc.group])
 }
