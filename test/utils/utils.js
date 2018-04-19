c40555291e3177f06666a3a07084f2bd73e0e183
Alex Feyerke <espy@users.noreply.github.com>
2018-04-19 16:29:30 +0200

    feat(deprecate-nodejs): send issue with updated branch

--- test/utils/utils.js
+++ test/utils/utils.js
@@ -7,7 +7,6 @@ const {
   generateGitHubCompareURL,
   getNodeVersionsFromTravisYML,
   addNodeVersionToTravisYML,
-  removeNodeVersionFromTravisYML,
   addNewLowestAndDeprecate
 } = require('../../utils/utils')
 
