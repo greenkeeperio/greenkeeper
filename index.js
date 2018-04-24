521e72b9ad8c3fe366c876102c84272ecad30958
Alex Feyerke <espy@users.noreply.github.com>
2018-04-24 11:39:31 +0200

    feat(index): add node jobs to jobs without  owners

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ require('./lib/rollbar')
 
   async function consume (job) {
     const data = JSON.parse(job.content.toString())
-    const jobsWithoutOwners = ['registry-change', 'stripe-event', 'schedule-stale-initial-pr-reminders', 'reset', 'cancel-stripe-subscription']
+    const jobsWithoutOwners = ['registry-change', 'stripe-event', 'schedule-stale-initial-pr-reminders', 'reset', 'cancel-stripe-subscription', 'update-nodejs-version', 'deprecate-nodejs-version']
     if (jobsWithoutOwners.includes(data.name) || data.type === 'marketplace_purchase') {
       return queueJob(data.name, job)
     }
