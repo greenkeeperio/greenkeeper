99caaf2ee032f9a3b72f21ff473238d08d9774a3
Alex Feyerke <espy@users.noreply.github.com>
2018-04-25 16:38:43 +0200

    feat(update-node): more details in issue text

--- content/nodejs-release-issue.js
+++ content/nodejs-release-issue.js
@@ -25,9 +25,21 @@ module.exports = ({owner, repo, base, head, nodeVersion, codeName, travisModifie
   return md`
 ## Version ${nodeVersion} of Node.js (code name ${codeName}) has been released! 🎊
 
-Greenkeeper has already created a branch with the following changes:
+To see what happens to your code in Node.js ${nodeVersion}, Greenkeeper has created a branch with the following changes:
 ${showTravisMessage(travisModified)}${showNVMRCMessage(nvmrcModified)}${showEngineTransformMessages(engineTransformMessages)}
-If you’re interested in upgrading this repo to Node.js ${nodeVersion}, you can <a href="${compareURL}">open a PR with these changes</a>.
+If you’re interested in upgrading this repo to Node.js ${nodeVersion}, you can <a href="${compareURL}">open a PR with these changes</a>. Please note that this issue is just intended as a friendly reminder and the PR as a possible starting point for getting your code running on Node.js ${nodeVersion}.
+
+<details>
+<summary>More information on this issue</summary>
+
+Greenkeeper has checked the \`engines\` key in any \`package.json\` file, the \`.nvmrc\` file, and the \`.travis.yml\` file, if present.
+- \`engines\` was only updated if it defined a single version, not a range.
+- \`.nvmrc\` was updated to Node.js ${nodeVersion}
+- \`.travis.yml\` was only changed if there was a root-level \`node_js\` that didn’t already include Node.js ${nodeVersion}, such as \`node\` or \`lts/*\`. In this case, the new version was appended to the list. We didn’t touch job or matrix configurations because these tend to be quite specific and complex, and it’s difficult to infer what the intentions were.
+
+For many simpler \`.travis.yml\` configurations, this PR should suffice as-is, but depending on what you’re doing it may require additional work or may not be applicable at all. We’re also aware that you may have good reasons to not update to Node.js ${nodeVersion}, which is why this was sent as an issue and not a pull request. Feel free to delete it without comment, I’m a humble robot and won’t feel rejected :robot:
+
+</details>
 
 ---
 
