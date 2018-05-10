327ee82411d63632c2c8148301c1ebf83235a908
Finn Pauls <derfinn@gmail.com>
2018-05-10 19:15:18 +0200

    fix(create-version-branch): check with github if pr is actually open fixes #752

--- jobs/create-version-branch.js
+++ jobs/create-version-branch.js
@@ -192,18 +192,7 @@ module.exports = async function (
     }))
   }
 
-  const openPR = _.get(
-    await repositories.query('pr_open_by_dependency', {
-      key: [repositoryId, dependencyKey],
-      include_docs: true
-    }),
-    'rows[0].doc'
-  )
-  if (openPR) {
-    log.info('database: found open PR for this dependency', {repositoryId, dependencyKey, openPR})
-  } else {
-    log.info('database: no open PR for this dependency', {repositoryId, dependencyKey})
-  }
+  const openPR = await findOpenPR()
 
   const commitMessageKey = !satisfies && type === 'dependencies'
     ? 'dependencyUpdate'
@@ -370,6 +359,29 @@ module.exports = async function (
       owner,
       repo
     }))
+  }
+
+  async function findOpenPR () {
+    const openPR = _.get(
+      await repositories.query('pr_open_by_dependency', {
+        key: [repositoryId, dependencyKey],
+        include_docs: true
+      }),
+      'rows[0].doc'
+    )
+
+    if (!openPR) return false
+    log.info(`database: found open PR for ${dependencyKey}`, {openPR})
+
+    const pr = await ghqueue.read(github => github.pullRequests.get({
+      owner,
+      repo,
+      number: openPR.number
+    }))
+    if (pr.state === 'open') return openPR
+
+    await upsert(repositories, openPR._id, _.pick(pr, ['state', 'merged']))
+    return false
   }
 }
 
