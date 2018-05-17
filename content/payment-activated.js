0d72e09150d2d6d51055b64449ba5535fe86899c
Jan Lehnardt <jan@apache.org>
2018-05-17 12:24:19 +0200

    feat(enterprise): do not handle billing for enterprise

--- content/payment-activated.js
+++ content/payment-activated.js
@@ -1,5 +1,11 @@
 const md = require('./template')
+const env = require('./env')
 
-module.exports = ({accountToken}) => md`💸 Payment has been activated 💸
-Enabling Greenkeeper on this repository by merging this pull request might increase your monthly payment. If you’re unsure, please [check your billing status](https://account.greenkeeper.io/status?token=${accountToken})
-`
+module.exports = ({accountToken}) => {
+  if (env.IS_ENTERPRISE) {
+    return ''
+  }
+
+  return md`💸 Payment has been activated 💸
+Enabling Greenkeeper on this repository by merging this pull request might increase your monthly payment. If you’re unsure, please [check your billing status](https://account.greenkeeper.io/status?token=${accountToken})`
+}
