5a96010420806ac0c626619fdd54c74db0ec68ca
Julia Krüger <juliakbln@gmail.com>
2018-05-18 15:57:24 +0200

    feat(content/update-PR): use the Monorepo dependency group name in PR content

--- content/update-pr.js
+++ content/update-pr.js
@@ -1,8 +1,10 @@
 const _ = require('lodash')
 const md = require('./template')
 
-module.exports = ({version, dependencyLink, dependency, oldVersionResolved, type, release, diffCommits}) => md`
-## Version **${version}** of ${dependencyLink} was just published.
+module.exports = ({version, dependencyLink, dependency, monorepoGroupName, release, diffCommits, oldVersionResolved, type}) => md`
+${_.isEmpty(monorepoGroupName)
+  ? `## Version **${version}** of ${dependencyLink} was just published.`
+  : `## Version **${version}** of ${monorepoGroupName} packages were just published.`}
 
 <table>
   <tr>
@@ -10,7 +12,7 @@ module.exports = ({version, dependencyLink, dependency, oldVersionResolved, type
       Dependency
     </th>
     <td>
-      <code>${dependency}</code>
+      <code>${monorepoGroupName || dependency}</code>
     </td>
   </tr>
   <tr>
@@ -35,7 +37,7 @@ The version **${version}** is **not covered** by your **current version range**.
 
 If you don’t accept this pull request, your project will work just like it did before. However, you might be missing out on a bunch of new features, fixes and/or performance improvements from the dependency update.
 
-It might be worth looking into these changes and trying to get this project onto the latest version of ${dependency}.
+It might be worth looking into these changes and trying to get this project onto the latest version of ${monorepoGroupName || dependency}.
 
 If you have a solid test suite and good coverage, a passing build is a strong indicator that you can take advantage of these changes directly by merging the proposed change into your project. If the build fails or you don’t have such unconditional trust in your tests, this branch is a great starting point for you to work on the update.
 
