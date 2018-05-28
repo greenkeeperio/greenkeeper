1b1fce49ae557151aa5d0947248f50f50115e6a8
Jan Lehnardt <jan@apache.org>
2018-05-28 16:27:11 +0200

    test: memoize getLogsDb()

---
+++ test/lib/dbs.js
@@ -0,0 +1,20 @@
+const lolex = require('lolex')
+const dbs = require('../../lib/dbs')
+
+describe('dbs access', () => {
+  test('getLogsDb memoization', async() => {
+    const clock = lolex.install()
+
+    const logsDb = dbs.getLogsDb()
+
+    // validate db instance is equal
+    const nextLogsDb = dbs.getLogsDb()
+    expect(nextLogsDb).toBe(logsDb)
+
+    // jump one month
+    clock.tick(60 * 60 * 24 * 31 * 1000)
+    const nextMonthsLogsDb = dbs.getLogsDb()
+    expect(nextMonthsLogsDb).not.toBe(logsDb)
+    clock.uninstall()
+  })
+})
