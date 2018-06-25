6092ebad69099f8fbb347dd5d491dccb6ff3ac38
Alex Feyerke <espy@users.noreply.github.com>
2018-06-25 17:50:06 +0200

    tests(invalid-config-file): use mockImplementation
    Is nicer than requireActual etc

--- test/jobs/invalid-config-file.js
+++ test/jobs/invalid-config-file.js
@@ -3,49 +3,34 @@
 describe('invalid-config-file', async () => {
   beforeEach(() => {
     jest.resetModules()
+    jest.mock('../../lib/gk-kit')
   })
 
   jest.mock('../../lib/dbs')
-  const dbs = require('../../lib/dbs')
-
-  // Remove all DB calls
-  const noop = () => ''
-  dbs.mockImplementation(() => ({
-    installations: {
-      get: noop()
-    },
-    repositories: {
-      get: noop(),
-      query: noop()
-    }
-  }))
 
   test('create new issue', async () => {
     expect.assertions(6)
-    jest.mock('../../lib/gk-kit', (accountId) => (accountId) => {
-      expect(accountId).toEqual('2020')
-      const lib = require.requireActual('../../lib/gk-kit')
-      lib.repositories = (repositoryId) => {
-        expect(repositoryId).toEqual('invalid-config1')
-        return {
-          issues: {
-            getInvalidConfigIssues: () => {
-              return []
-            },
-            create: (title, body, issueDoc) => {
-              expect(title).toEqual('Invalid Greenkeeper configuration file')
-              expect(body).toMatch('The group name `#invalid#groupname#` is invalid. Group names may only contain alphanumeric characters and underscores (a-zA-Z_).')
-              expect(body).not.toMatch(/which is preventing Greenkeeper from opening its initial pull request/)
-              expect(issueDoc).toMatchObject({
-                initial: false,
-                invalidConfig: true
-              })
-            }
+    const kit = require('../../lib/gk-kit')
+
+    kit.mockImplementation((accountId) => ({
+      repositories: (repositoryId) => ({
+        issues: {
+          getInvalidConfigIssues: () => [],
+          create: (title, body, issueDoc) => {
+            expect(accountId).toEqual('2020')
+            expect(repositoryId).toEqual('invalid-config1')
+            expect(title).toEqual('Invalid Greenkeeper configuration file')
+            expect(body).toMatch('The group name `#invalid#groupname#` is invalid. Group names may only contain alphanumeric characters and underscores (a-zA-Z_).')
+            expect(body).not.toMatch(/which is preventing Greenkeeper from opening its initial pull request/)
+            expect(issueDoc).toMatchObject({
+              initial: false,
+              invalidConfig: true
+            })
           }
         }
-      }
-      return lib
-    })
+      })
+    }))
+
     const invalidConfigFile = require('../../jobs/invalid-config-file')
 
     await invalidConfigFile({
