7471267748acbc88414c4a3fc00597a0b73dc36b
Alex Feyerke <espy@users.noreply.github.com>
2018-06-26 10:23:01 +0200

    chore(invalid-config-file): clean up documentation

--- jobs/invalid-config-file.js
+++ jobs/invalid-config-file.js
@@ -1,18 +1,25 @@
 /*
 
-Invalid Config File Issue
+# Invalid Config File Issue
 
 Generates an issue if the greenkeeper.json config file in the
 repo is invalid, and also saves this issue in our DB.
 
-It takes the following arguments:
+## Arguments:
 
-repositoryId:Number: eg. 7914533, the GitHub repo ID. Might be a string.
-accountId:Number: eg. , the GitHub account ID. Might be a string
-messages:Array of strings: The validation error messages to be displayed in the issue
-isBlockingInitialPR:Boolean: Adds extra info to the comment text to make clear that Greenkeeper can’t work until the problem is fixed
+repositoryId        :Number
+  > eg. 7914533, the GitHub repo ID. Might be a string.
 
-Outputs/Effects:
+accountId           :Number
+  > eg. 391124, the GitHub account ID. Might be a string.
+
+messages            :Array of strings
+  > The validation error messages to be displayed in the issue.
+
+isBlockingInitialPR :Boolean
+  > Adds extra info to the issue text to make clear that Greenkeeper can’t work until the problem is fixed.
+
+## Outputs/Effects:
 
 Unless we already have an issue of this type on the repo, opens an issue and adds it to our DB as well.
 
