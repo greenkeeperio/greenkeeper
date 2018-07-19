312de2121bd2cf02e94e1cde2754a4074d226a21
Jan Lehnardt <jan@apache.org>
2018-07-19 16:49:39 +0200

    fix: allow log db rollover. Closes #838

--- test/lib/dbs.js
+++ test/lib/dbs.js
@@ -3,18 +3,25 @@ const dbs = require('../../lib/dbs')
 
 describe('dbs access', () => {
   test('getLogsDb memoization', async () => {
-    const clock = lolex.install()
+    const clock = lolex.install({now: new Date()})
 
     const logsDb = dbs.getLogsDb()
 
     // validate db instance is equal
     const nextLogsDb = dbs.getLogsDb()
+    // we should use nextLogsDb.name here, but a Node bug
+    // in Proxys prevents us from getting at the original
+    // value here.
     expect(nextLogsDb).toBe(logsDb)
 
     // jump one month
-    clock.tick(60 * 60 * 24 * 31 * 1000)
+    clock.tick(60 * 60 * 24 * 45 * 1000)
     const nextMonthsLogsDb = dbs.getLogsDb()
+    // we should use nextLogsDb.name here, but a Node bug
+    // in Proxys prevents us from getting at the original
+    // value here.
     expect(nextMonthsLogsDb).not.toBe(logsDb)
+
     clock.uninstall()
   })
 })
