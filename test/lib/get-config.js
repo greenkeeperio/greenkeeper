562b52fe92044b60ee0884713a2f18e1b2bea3b5
hulkoba <fuchspippi@web.de>
2018-07-24 13:41:13 +0200

    test(get-config): detect custom pr title

--- test/lib/get-config.js
+++ test/lib/get-config.js
@@ -126,6 +126,49 @@ test('get custom commit message', () => {
   expect(getConfig(repository)).toEqual(expected)
 })
 
+test('get custom pr title', () => {
+  expect.assertions(1)
+
+  const repository = {
+    packages: {
+      'package.json': {
+        greenkeeper: {
+          commitMessages: {
+            initialBadge: 'HELLO Greenkeeper badge'
+          },
+          prTitles: {
+            basicPR: 'update Jacoba to the latest version',
+            groupPR: 'update group of Jacoba to the latest verion'
+          }
+        }
+      }
+    }
+  }
+
+  const expected = {
+    label: 'greenkeeper',
+    branchPrefix: 'greenkeeper/',
+    ignore: [],
+    commitMessages: {
+      addConfigFile: 'chore: add Greenkeeper config file',
+      updateConfigFile: 'chore: update Greenkeeper config file',
+      initialBadge: 'HELLO Greenkeeper badge',
+      initialDependencies: 'chore(package): update dependencies',
+      initialBranches: 'chore(travis): whitelist greenkeeper branches',
+      dependencyUpdate: 'fix(package): update ${dependency} to version ${version}',
+      devDependencyUpdate: 'chore(package): update ${dependency} to version ${version}',
+      dependencyPin: 'fix: pin ${dependency} to ${oldVersion}',
+      devDependencyPin: 'chore: pin ${dependency} to ${oldVersion}',
+      closes: '\n\nCloses #${number}'
+    },
+    prTitles: {
+      basicPR: 'update Jacoba to the latest version',
+      groupPR: 'update group of Jacoba to the latest verion'
+    }
+  }
+  expect(getConfig(repository)).toEqual(expected)
+})
+
 test('get ignore config with empty greenkeeper config', () => {
   expect.assertions(1)
 
