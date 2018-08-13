0b9e23a3d0fdd7c89302430d3610b8af9c67fd35
Jan Lehnardt <jan@apache.org>
2018-08-13 15:11:43 +0200

    wip: use lockfile server

--- jobs/create-version-branch.js
+++ jobs/create-version-branch.js
@@ -19,7 +19,11 @@ const {
 } = require('../lib/monorepo')
 
 const { getActiveBilling, getAccountNeedsMarketplaceUpgrade } = require('../lib/payments')
-const { createTransformFunction, generateGitHubCompareURL, hasTooManyPackageJSONs } = require('../utils/utils')
+const { createTransformFunction,
+  generateGitHubCompareURL,
+  hasTooManyPackageJSONs,
+  createLockfileTransformFunction
+} = require('../utils/utils')
 
 const prContent = require('../content/update-pr')
 
@@ -95,9 +99,9 @@ module.exports = async function (
   const satisfies = semver.satisfies(version, oldVersion)
   const hasModuleLockFile = repository.files && isTrue(repository.files['npm-shrinkwrap.json'])
   const hasProjectLockFile = repository.files && (isTrue(repository.files['package-lock.json']) || isTrue(repository.files['yarn.lock']))
-  const usesGreenkeeperLockfile = repository.packages['package.json'] &&
-    repository.packages['package.json'].devDependencies &&
-    _.some(_.pick(repository.packages['package.json'].devDependencies, 'greenkeeper-lockfile'))
+  // const usesGreenkeeperLockfile = repository.packages['package.json'] &&
+  //   repository.packages['package.json'].devDependencies &&
+  //   _.some(_.pick(repository.packages['package.json'].devDependencies, 'greenkeeper-lockfile'))
   // Bail if it’s in range and the repo uses shrinkwrap
   if (satisfies && hasModuleLockFile) {
     log.info(`exited: ${dependency} ${version} satisfies semver & repository has a module lockfile (shrinkwrap type)`)
@@ -106,10 +110,10 @@ module.exports = async function (
 
   // If the repo does not use greenkeeper-lockfile, there’s no point in continuing because the lockfiles
   // won’t get updated without it
-  if (satisfies && hasProjectLockFile && !usesGreenkeeperLockfile) {
-    log.info(`exited: ${dependency} ${version} satisfies semver & repository has a project lockfile (*-lock type), and does not use gk-lockfile`)
-    return
-  }
+  // if (satisfies && hasProjectLockFile && !usesGreenkeeperLockfile) {
+  //   log.info(`exited: ${dependency} ${version} satisfies semver & repository has a project lockfile (*-lock type), and does not use gk-lockfile`)
+  //   return
+  // }
 
   // Some users may want to keep the legacy behaviour where all lockfiles are only ever updated on out-of-range updates.
   const config = getConfig(repository)
@@ -195,17 +199,29 @@ module.exports = async function (
         commitMessage += getMessage(config.commitMessages, 'closes', {number: openPR.number})
       }
       log.info('commit message created', {commitMessage})
-      return {
+      let transformFuns = []
+
+      const hasLockfile = !!(json.packages['package-lock.json'] || json.packages['yarn.lock'])
+      if (hasLockfile) {
+        const isNpm = !!json.packages['package-lock.json']
+        transformFuns.push({
+          transform: createLockfileTransformFunction(dependencyType, depName, version, log),
+          path: isNpm ? 'package-lock.json' : 'yarn.lock',
+          message: commitMessage
+        })
+      }
+
+      transformFuns.push({
         transform: createTransformFunction(dependencyType, depName, version, log),
         path: 'package.json',
         message: commitMessage
-      }
+      })
     }))
   }
 
   const openPR = await findOpenPR()
 
-  const transforms = _.compact(await createTransformsArray(group, repository.packages['package.json']))
+  const transforms = _.compact(_.flatten(await createTransformsArray(group, repository.packages['package.json'])))
   const sha = await createBranch({
     installationId,
     owner,
