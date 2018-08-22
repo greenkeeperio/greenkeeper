3f1b503f7fda80f4807c7712524f4b5aeb670e67
Alex Feyerke <espy@users.noreply.github.com>
2018-08-22 15:01:25 +0200

    feat(cisb): generate lockfiles for initial subgroup branch

--- test/jobs/create-initial-subgroup-branch.js
+++ test/jobs/create-initial-subgroup-branch.js
@@ -27,7 +27,7 @@ describe('create initial subgroup branch', () => {
     const { installations, repositories } = await dbs()
     await Promise.all([
       removeIfExists(installations, '123'),
-      removeIfExists(repositories, '1111', '1111:branch:1234abcd', '1112', '1112:branch:1234abcd', 'initialSubgroupBranch3', 'initialSubgroupBranch3:branch:1234abcd')
+      removeIfExists(repositories, '1111', '1111:branch:1234abcd', '1112', '1112:branch:1234abcd', 'initialSubgroupBranch3', 'initialSubgroupBranch3:branch:1234abcd', '1111-lockfiles', '1111-lockfiles:branch:1234abcd')
     ])
   })
 
@@ -586,6 +586,144 @@ describe('create initial subgroup branch', () => {
     expect(newJob).toBeFalsy()
   })
 
+  test('create a subgroup branch with lockfiles', async () => {
+    const { repositories } = await dbs()
+    const configFileContent = {
+      groups: {
+        frontend: {
+          packages: [
+            'packages/frontend/package.json',
+            'packages/lalalalala/package.json'
+          ]
+        },
+        backend: {
+          packages: [
+            'packages/backend/package.json'
+          ]
+        }
+      }
+    }
+    await repositories.put({
+      _id: '1111-lockfiles',
+      fullName: 'hans/monorepo',
+      accountId: '123',
+      enabled: true,
+      headSha: 'hallo',
+      packages: {
+        'packages/frontend/package.json': {
+          name: 'testpkg',
+          dependencies: {
+            lodash: '^1.0.0'
+          }
+        },
+        'packages/backend/package.json': {
+          name: 'testpkg',
+          dependencies: {
+            lodash: '^1.0.0'
+          }
+        },
+        'packages/lalalalala/package.json': {
+          name: 'testpkg',
+          dependencies: {
+            lodash: '^1.0.0'
+          }
+        }
+      },
+      greenkeeper: configFileContent,
+      files: {
+        'package.json': ['frontend/package.json', 'backend/package.json', 'lalalalala/package.json'],
+        'package-lock.json': ['frontend/package-lock.json', 'backend/package-lock.json', 'lalalalala/package-lock.json'],
+        'npm-shrinkwrap.json': [],
+        'yarn.lock': []
+      }
+    })
+
+    expect.assertions(12)
+
+    const httpRequests = nock('https://api.github.com')
+      .post('/installations/37/access_tokens')
+      .optionally()
+      .reply(200, {
+        token: 'secret'
+      })
+      .get('/rate_limit')
+      .optionally()
+      .reply(200, {})
+      .get('/repos/hans/monorepo/contents/greenkeeper.json')
+      .reply(200, {
+        type: 'file',
+        path: 'greenkeeper.json',
+        name: 'greenkeeper.json',
+        content: Buffer.from(JSON.stringify(configFileContent)).toString('base64')
+      })
+      .get('/repos/hans/monorepo/contents/packages/frontend/package.json')
+      .reply(200, {
+        path: 'packages/frontend/package.json',
+        name: 'package.json',
+        content: encodePkg({
+          name: 'testpkg',
+          dependencies: {
+            lodash: '^1.0.0'
+          }
+        })
+      })
+      .get('/repos/hans/monorepo/contents/packages/lalalalala/package.json')
+      .reply(200, {
+        path: 'packages/lalalalala/package.json',
+        name: 'package.json',
+        content: encodePkg({
+          name: 'testpkg',
+          dependencies: {
+            lodash: '^1.0.0'
+          }
+        })
+      })
+      .get('/repos/hans/monorepo')
+      .reply(200, {
+        default_branch: 'master'
+      })
+
+    const npmHttpRequests = nock('https://registry.npmjs.org')
+      .get('/lodash')
+      .reply(200, {
+        'dist-tags': {
+          latest: '3.0.0-rc1'
+        },
+        versions: {
+          '2.0.0-rc1': true,
+          '2.0.0-rc2': true,
+          '2.0.0': true,
+          '3.0.0-rc1': true,
+          '1.0.0': true
+        }
+      })
+
+    // mock relative dependencies
+    jest.mock('../../lib/create-branch', () => ({ transforms, processLockfiles, lockFileCommitMessage, repoDoc }) => {
+      expect(processLockfiles).toBeTruthy()
+      expect(repoDoc).toHaveProperty('files')
+      expect(lockFileCommitMessage).toEqual('chore(package): update lockfile')
+      transforms.forEach(t => {
+        const newPkg = JSON.parse(
+          t.transform(JSON.stringify({ dependencies: { lodash: '^1.0.0' } }))
+        )
+        expect(newPkg.dependencies['lodash']).toEqual('^2.0.0')
+      })
+
+      return '1234abcd'
+    })
+    const createInitialSubgroupBranch = require('../../jobs/create-initial-subgroup-branch')
+    const newJob = await createInitialSubgroupBranch({repositoryId: '1111-lockfiles', groupName: 'frontend'})
+    const newBranch = await repositories.get('1111-lockfiles:branch:1234abcd')
+
+    expect(httpRequests.isDone()).toBeTruthy()
+    expect(npmHttpRequests.isDone()).toBeTruthy()
+    expect(newJob).toBeFalsy() // This only creates branches
+    expect(newBranch.type).toEqual('branch')
+    expect(newBranch.initial).toBeFalsy()
+    expect(newBranch.subgroupInitial).toBeTruthy()
+    expect(newBranch.repositoryId).toEqual('1111-lockfiles')
+  })
   function encodePkg (pkg) {
     return Buffer.from(JSON.stringify(pkg)).toString('base64')
   }
