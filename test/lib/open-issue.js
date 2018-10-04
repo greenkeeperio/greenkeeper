9d06086f048c928d9f5b405711ce86d43ad88330
hulkoba <fuchspippi@web.de>
2018-10-04 15:35:18 +0200

    chore(test) re-add nock variable

--- test/lib/open-issue.js
+++ test/lib/open-issue.js
@@ -57,7 +57,7 @@ test('open-issue', async () => {
   })
   const openIssue = require('../../lib/open-issue')
 
-  nock('https://api.github.com')
+  const gitHubNock = nock('https://api.github.com')
     .post('/app/installations/123/access_tokens')
     .optionally()
     .reply(200, {
