6672775c00277a5c2499b077fe528b4bee02e6bb
Alex Feyerke <espy@users.noreply.github.com>
2018-10-16 14:44:12 +0200

    fix(github): handle event payloads properly

--- jobs/github-event/check_run/completed.js
+++ jobs/github-event/check_run/completed.js
@@ -8,14 +8,14 @@ Docs: https://developer.github.com/v3/activity/events/types/#checkrunevent
 This is the handler for the`completed` action, which according to the docs doesn’t exist for this endpoint, but according to reality actually does.
 */
 
-const _ = require('lodash')
 const onBranchStatus = require('../../../lib/on-branch-status')
 
-module.exports = async function ({ status, conclusion, head_sha, repository, installation }) { // eslint-disable-line
+module.exports = async function ({ check_run, repository, installation }) { // eslint-disable-line
+  const { status, conclusion, head_sha } = check_run // eslint-disable-line
   // This shouldn’t be possible, since this is the completed event handler, but hey.
   if (status !== 'completed') return
   // The status of this particular check_run is inconclusive (we can’t say whether the
   // build is passing or failing), so there’s no point in continuing
-  if (_.includes(['cancelled', 'timed_out', 'action_required'], status)) return
+  if (['cancelled', 'timed_out', 'action_required'].includes(conclusion)) return
   return onBranchStatus(repository, head_sha, installation)
 }
