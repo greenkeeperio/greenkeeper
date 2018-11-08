2c5d0bb2f585751bba57d879bd6876a3b0a0595d
hulkoba <fuchspippi@web.de>
2018-11-08 11:09:13 +0100

    doc(issue): change reset instructions. Now our customer should use now our customers should use the reset button

--- content/timeout-issue.js
+++ content/timeout-issue.js
@@ -2,7 +2,7 @@ const md = require('./template')
 const env = require('../lib/env')
 
 module.exports = ({ fullName }) =>
-  md`🚨 You need to enable Continuous Integration on all branches of this repository. 🚨
+  md`🚨 You need to enable Continuous Integration on **all** branches of this repository. 🚨
 
 To enable Greenkeeper, you need to make sure that a [commit status](https://help.github.com/articles/about-statuses/) is reported on all branches. This is required by Greenkeeper because it uses your CI build statuses to figure out when to notify you about breaking changes.
 
@@ -10,7 +10,7 @@ Since we didn’t receive a CI status on the ${branchLink(fullName)} branch, it�
 
 If you _have_ already set up a CI for this repository, you might need to check how it’s configured. Make sure it is set to run on all new branches. If you don’t want it to run on absolutely every branch, you can whitelist branches starting with ${md.code('greenkeeper/')}.
 
-Once you have installed and configured CI on this repository correctly, you’ll need to re-trigger Greenkeeper’s initial pull request. To do this, please delete the \`greenkeeper/initial\` branch in this repository, and then remove and re-add this repository to the Greenkeeper App’s white list on Github. You'll find this list on your repo or organization’s __settings__ page, under __Installed GitHub Apps__.
+Once you have installed and configured CI on this repository correctly, you’ll need to re-trigger Greenkeeper’s initial pull request. To do this, please click the 'fix repo' button on [account.greenkeeper.io](https.account.greenkeeper.io).
 `
 function branchLink (fullName) {
   return md.link(
