96009128ace1e58298a46621781c800084157483
Jan Lehnardt <jan@apache.org>
2019-03-14 11:43:02 +0100

    docs(payment-required): Update payment-required. pricing text
    Co-Authored-By: hulkoba <fuchspippi@web.de>

--- content/payment-required.js
+++ content/payment-required.js
@@ -6,5 +6,5 @@ module.exports = () =>
   Hello!
   You have set your repository to private. From now on Greenkeeper is no longer free. We have disabled your repo for now.
   Please enter your payment information at [account.greenkeeper.io](https://account.greenkeeper.io).
-  If you want to know what you have to pay from now on, check [https://greenkeeper.io/#pricing](https://greenkeeper.io/#pricing).
+ For pricing information, please visit [https://greenkeeper.io/#pricing](https://greenkeeper.io/#pricing).
 `
